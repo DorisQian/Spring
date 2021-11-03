c40d203dc7d40415cf304ab6831d4507c89df24e
DorisQian <qianyh0806@163.com>
2021-11-04 01:51:45 +0800

    获取对象的方式，id和类名.class，分模块xml

--- spring_ioc/src/main/java/com/doris/web/UserController.java
+++ spring_ioc/src/main/java/com/doris/web/UserController.java
@@ -4,6 +4,7 @@ import com.doris.service.UserService;
 import com.doris.service.impl.UserServiceImpl;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class UserController {
     public static void main(String[] args) {
@@ -11,7 +12,10 @@ public class UserController {
 //        userService.save();
 //        依赖注入
         ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
+//        ApplicationContext app = new FileSystemXmlApplicationContext("/Users/doris/study/java/Spring/spring_ioc/src/main/resources/applicationContext.xml");
         UserService userService = (UserService) app.getBean("userService");
+//        配置文件UserService只存在一个bean可以用该方式
+//        UserService userService1 = app.getBean(UserService.class);
         userService.save();
     }
 }
